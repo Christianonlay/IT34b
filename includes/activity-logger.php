9f9c25876e662253fa82dd0727e0b02fde471e90
javsomongos-spec <javsomongos@gmail.com>
2026-08-19 15:32:10 +0800

    Added contents for includes/activity-logger.php

---
+++ includes/activity-logger.php
@@ -0,0 +1,38 @@
+<?php 
+    function logActivity($pdo,$user_id,$email,$action,$status='success'){
+        try{
+            //get client ip address
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? 'Unknown';
+
+            // String to Array
+            if(strpos($ip,',') !==false){
+                $ip = trim(explode(',', $ip)[0]);
+            }
+
+            //Get user agent (browser)
+            $user_agent = substr($_server['HTTP_USER_AGENT']?? 'Unknown' ,0,255);
+        
+            //Apllication Querry #1
+            $stml = $pdo ->prepare("
+                INSERT INTO activity_logs(
+                user_id,
+                user_email,
+                activity_log_action,
+                actiivity_log_status,
+                activity_log_user_agent
+                ) VALUES (?,?,?,?,?,?)
+                ");
+
+        } catch (PDOException $e){
+            error_log("Activity Log Error: " . $e->getMessage());
+            return false;
+        }
+    
+            
+        
+
+
+    }
+
+
+    ?>
