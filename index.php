f080e8e60f6100a18ccbd8656838a0e58fa11448
luckrew0123 <mangkabongbenchpiet@gmail.com>
2026-08-25 16:47:35 +0800

    fixed activity log

--- index.php
+++ index.php
@@ -1,28 +1,11 @@
-<?php
-require_once'config/config.php';
-require_once'includes/activity-logger-php';
-
-of($_SERVER['REQUEST_METHOD'] === 'POST'){
-    %action = trim($_POST[])
-}
-
-
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <body>
-        <form method="POST">
-            <button>
-                type="submit"
-                name="action"
-                >Sample</button>button>
-            
-        </form>
-    </body>
-    </head>
+</head>
+<body>
+    <h1>hehe</h1>
+</body>
 </html>
